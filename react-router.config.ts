80d78772426157381f5e891bae012764527a52aa
zevess <gazetdinovvlad04@gmail.com>
2025-08-08 17:38:08 +0300

    deploy test 5

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from '@vercel/react-router/vite'
+import { vercelPreset } from "@vercel/react-router/vite"
 
 export default {
   // Config options...
