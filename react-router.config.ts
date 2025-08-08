3b9aa8fb873ba78002f07ffb72276d66a0793259
zevess <gazetdinovvlad04@gmail.com>
2025-08-08 13:46:32 +0300

    dependencies reinstall

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,6 @@ import { vercelPreset } from '@vercel/react-router/vite'
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
   presets: [vercelPreset()]
 } satisfies Config;
